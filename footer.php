2de56af097bf6ce9f27b220660b2412b292f2f29
noobalpha <31156685+shelcia@users.noreply.github.com>
2020-07-03 03:25:34 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style.css" />
+        <title>footer</title>
+        <style>
+                .footerhere {
+                        width: 100%;
+                        position: absolute;
+                        bottom: 0px;
+                        background-color: #f5f5f5;
+                        color: gray;
+                }
+                .footerhere h4{
+                        padding:20px;
+                }
+                body {
+                        margin: 0px;
+                        padding: 0px;
+                        box-sizing: border-box;
+                        font-family: "Poppins", sans-serif;
+                }
+        </style>
+</head>
+<body>
+        <div class="footerhere">
+                <h4 class="text-center">developed by shelcia</h4>
+        </div>
+</body>
+</html>
